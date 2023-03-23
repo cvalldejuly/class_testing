07a027236978e92cabbc18629edcac0ee34fa0ff
cvalldejuly <cameliav@uchicago.edu>
2023-03-23 17:04:39 -0500

    Update class_1_testing.py

--- class_1_testing.py
+++ class_1_testing.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Spyder Editor
 
-@author: cclapp
+This is a temporary script file.
 """
 
-print("Hello, World!!!")
+print("Hello, World")
 
-print("No more math.")
+x = 10 
 
+print(x*2)
